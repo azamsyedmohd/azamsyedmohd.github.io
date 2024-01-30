d0600cbaca40a5750fc00d32de6bf8b52d20ba3f
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-30 23:31:04 +0530

    NavBar Component Created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+const NavBar = () => {
+  return (
+    <>
+      <section className="fixed w-full h-[50px] flex justify-between items-center px-4 bg-[#a5b4fc]"></section>
+    </>
+  );
+};
+export default NavBar;
