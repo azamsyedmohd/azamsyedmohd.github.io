c5386f096d5a2d2a874414d2e019998c9c430b9e
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-31 00:37:51 +0530

    Profile Photo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,16 @@
+import Profile from "../assets/Profile Image.jpeg";
 const NavBar = () => {
   return (
     <>
-      <section className="fixed w-full h-[50px] flex justify-between items-center px-4 bg-[#a5b4fc]"></section>
+      <section className="fixed w-full  flex justify-between items-center px-4 py-2 bg-[#a5b4fc]">
+        <article>
+          <img
+            className="w-[70px] h-[70px] rounded-lg bg-[#a5b4fc]"
+            src={Profile}
+            alt="Name-Logo"
+          />
+        </article>
+      </section>
     </>
   );
 };
