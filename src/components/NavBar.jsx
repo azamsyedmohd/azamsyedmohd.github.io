5754d9d659716207d101416de04138fb404e1755
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-02 05:56:14 +0530

    Header Component Completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   const [toggle, setToggle] = useState(true);
   return (
     <>
-      <section className=" w-full  flex justify-between items-center px-4 py-2 bg-[#a5b4fc] z-50">
+      <section className=" w-full  flex justify-between items-center px-12 py-5 bg-[#a5b4fc] z-50">
         <p className="font-caveat text-white text-4xl ml-3">
           <span className="pr-1">Syed</span>
           <span className="pr-1 ">Mohd</span>
