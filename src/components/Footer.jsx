778fdea67fb27ef8e3030e05a7dede1dcb78fb60
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-02 21:26:54 +0530

    Top Button animation added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
             <span className="pr-1">Azam</span>
             <Link to="/">
               <span
-                className="p-2 bg-white text-[#a5b4fc] rounded-full ml-4 hover:cursor-pointer font-semibold"
+                className="p-2 bg-white text-[#a5b4fc] rounded-full ml-4 hover:cursor-pointer font-semibold animate-bounce inline-block "
                 onClick={() => scrollToTop()}
               >
                 Top
