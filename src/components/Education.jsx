f7cff29ec47f9a6041fc5a6eb2ffd1c70010e008
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-02 05:37:52 +0530

    Education Component became responsive

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,9 +6,9 @@ const Education = () => {
         <section className="text-4xl w-9/12 mx-auto lg:text-5xl text-white underline underline-offset-8 mb-20">
           Education
         </section>
-        <section className="w-9/12 mx-auto flex justify-center items-center">
-          <section className="flex flex-row w-9/12 mx-auto  p-5">
-            <article className="w-[400px] h-96 rounded-xl ">
+        <section className="w-9/12 container mx-auto flex flex-col lg:flex-row lg:justify-center lg:items-center">
+          <section className="flex flex-row w-9/12 mx-auto  p-5 hover:scale-125">
+            <article className="w-[400px] h-[430px] rounded-xl p-1">
               <section className="h-2/5 bg-white flex justify-center items-center rounded-t-xl">
                 <img
                   src={AMU}
@@ -17,24 +17,23 @@ const Education = () => {
                 />
               </section>
               <section className="h-3/5 p-4 flex flex-col gap-5  border-2 rounded-b-xl hover:cursor-pointer">
-                <p className="text-white text-lg font-semibold">
+                <p className="text-white text-sm lg:text-lg font-semibold">
                   Aligarh Muslim University, Aligarh
                 </p>
-                <p className="text-white text-lg font-semibold">
+                <p className="text-white text-sm lg:text-lg font-semibold">
                   Master of Computer Application ( MCA )
                 </p>
-                <p className="text-white text-lg font-semibold">
+                <p className="text-white text-sm lg:text-lg font-semibold">
                   Grade: 7.75 CGPA
                 </p>
-                <p className="text-white text-lg font-semibold">
+                <p className="text-white text-sm lg:text-lg font-semibold">
                   Passed with First Division
                 </p>
               </section>
             </article>
-            <article></article>
           </section>
-          <section className="flex flex-row w-9/12 mx-auto  p-5">
-            <article className="w-[400px] h-96 rounded-xl ">
+          <section className="flex flex-row w-9/12 mx-auto  p-5 hover:scale-125">
+            <article className="w-[400px] h-[430px] rounded-xl ">
               <section className="h-2/5 bg-white flex justify-center items-center rounded-t-xl  cursor-pointer">
                 <img
                   src={AMU}
@@ -43,21 +42,20 @@ const Education = () => {
                 />
               </section>
               <section className="h-3/5 p-4 flex flex-col gap-5 bg-green border-2 rounded-b-xl hover:scale-120 hover:cursor-pointer ">
-                <p className="text-white text-lg font-semibold ">
+                <p className="text-white text-sm lg:text-lg font-semibold ">
                   Aligarh Muslim University, Aligarh
                 </p>
-                <p className="text-white text-lg font-semibold ">
+                <p className="text-white text-sm lg:text-lg font-semibold ">
                   Bachelor of Science in Computer Application
                 </p>
-                <p className="text-white text-lg font-semibold ">
+                <p className="text-white text-sm  lg:text-lg font-semibold ">
                   Percentage: 75.34 %
                 </p>
-                <p className="text-white text-lg font-semibold">
+                <p className="text-white text-sm lg:text-lg font-semibold">
                   Passed with First Division
                 </p>
               </section>
             </article>
-            <article></article>
           </section>
         </section>
       </section>
