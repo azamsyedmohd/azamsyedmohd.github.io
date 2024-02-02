9b8bb4e277e76b4a7d6a9ede6462d7911d133f35
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-02 05:54:40 +0530

    Footer Component Created and Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
@@ -14,6 +14,7 @@ function App() {
       <Projects />
       <Education />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <>
+      <section className="bg-[#a5b4fc] py-6 px-16 flex justify-between  items-center gap-20">
+        <h1 className="text-white text-2xl font-semibold tracking-wider">
+          &#169; No Copyright
+        </h1>
+        <h1 className="text-white text-2xl font-semibold tracking-wider">
+          Made with Love 🧡!!
+        </h1>
+
+        <section>
+          <p className="font-caveat text-white text-4xl ml-3">
+            <span className="pr-1">Syed</span>
+            <span className="pr-1 ">Mohd</span>
+            <span className="pr-1">Azam</span>
+          </p>
+        </section>
+      </section>
+    </>
+  );
+};
+export default Footer;
