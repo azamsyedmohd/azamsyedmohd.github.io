15bf0bbb94ad28be194bed69a26c6016d498b4af
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-07 21:08:28 +0530

    images added

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+import YouTube from "../assets/Youtube-Scrrenshot.png";
+import Swiggy from "../assets/Swiggy.png";
+const projects = [
+  {
+    name: "ReactTube",
+    src: YouTube,
+    link: "https://tangerine-sawine-d8fbd2.netlify.app/",
+    code: "https://github.com/Syed-Mohd-Azam/ReactTube--Mini-Youtube-Application",
+  },
+  {
+    name: "SnackSwift",
+    src: Swiggy,
+    link: "https://marvelous-boba-04d9c8.netlify.app/",
+    code: "https://github.com/Syed-Mohd-Azam/SnackSwift---Mini-Swiggy-Application",
+  },
+];
+export default projects;
