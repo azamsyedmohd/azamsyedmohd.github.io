4dc6f349c00beafe731f595131b3ecd2f5db0e80
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-07 21:45:40 +0530

    Projects Component Completed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,25 +1,21 @@
-import Youtube from "../assets/Youtube-Scrrenshot.png";
-const ProjectCard = () => {
+// eslint-disable-next-line react/prop-types
+const ProjectCard = ({ project }) => {
+  // eslint-disable-next-line react/prop-types
+  const { name, src, code, link } = project;
   return (
     <>
-      <section className="w-80 h-80 hover:scale-110 ">
+      <section className="w-96 h-80 hover:scale-110 ">
         <article className="w-full h-3/6 p-1 border-[#a5b4fc] border-2 rounded-t-xl">
-          <img
-            className="w-full h-full rounded-t-xl"
-            src={Youtube}
-            alt="Image "
-          />
+          <img className="w-full h-full rounded-t-xl" src={src} alt="Image " />
         </article>
         <article className="w-full h-3/6 bg-[#a5b4fc] px-10 py-6 rounded-b-xl">
           <article className="w-full h-full">
-            <p className="text-white font-semibold text-2xl mb-10">
-              Project Name
-            </p>
+            <p className="text-white font-semibold text-2xl mb-10">{name}</p>
             <article className="flex flex-row justify-between">
-              <a href="">
+              <a href={code} target="_blank" rel="noreferrer">
                 <p className="text-white text-2xl font-semibold">{"</>"}</p>
               </a>
-              <a href="">
+              <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-white text-2xl font-semibold">Link</p>
               </a>
             </article>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable no-unused-vars */
 import ProjectCard from "./ProjectCard";
 import projects from "../utils/constants";
 const Projects = () => {
-  const projects =
+  const project =
     "Projects are the poetry of hard work, the prose of dedication, and the epics of achievement !!";
   return (
     <>
       <section className="w-full min-h-screen">
         <section className="w-9/12 mx-auto text-4xl text-[#a5b4fc] italic tracking-wide pt-24">
           <span>{"'"}</span>
-          <span className="leading-relaxed">{projects}</span>
+          <span className="leading-relaxed">{project}</span>
           <span>{"'"}</span>
         </section>
-        <section className="w-9/12 mx-auto  p-8 my-8 ">
+        <section className="w-9/12 mx-auto  p-8 my-8 flex flex-row grow  gap-20 justify-center">
           {projects?.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
