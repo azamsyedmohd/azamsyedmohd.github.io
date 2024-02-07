4aa32cc8dfdfef3720e7d6318a477b8c04542e55
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-07 23:37:31 +0530

    Projects Section Styling Added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     "Projects are the poetry of hard work, the prose of dedication, and the epics of achievement !!";
   return (
     <>
-      <section className="w-full min-h-screen">
+      <section className="w-full min-h-screen py-20">
         <section className="w-9/12 mx-auto pt-24">
           <span>{"'"}</span>
           <span className="text-lg md:2xl leading-relaxed lg:text-4xl text-[#a5b4fc] italic  ">
@@ -14,7 +14,7 @@ const Projects = () => {
           </span>
           <span>{"'"}</span>
         </section>
-        <section className="w-9/12 mx-auto  p-8 my-8 flex lg:flex-row lg:grow  lg:gap-20 justify-center flex-col gap-10">
+        <section className="w-9/12 mx-auto  p-8 my-8 flex lg:flex-row lg:grow  lg:gap-20 justify-center flex-col gap-10 pb-10">
           {projects?.map((project, index) => (
             <ProjectCard key={index} project={project} />
           ))}
