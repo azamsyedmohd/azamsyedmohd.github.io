c4f70054c8b55ae8f2fadc56eb2c43d6ad7b317c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-07 21:50:14 +0530

    Projects Name Declared

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,13 @@ import YouTube from "../assets/Youtube-Scrrenshot.png";
 import Swiggy from "../assets/Swiggy.png";
 const projects = [
   {
-    name: "ReactTube - Mini-YouTube-App",
+    name: "ReactTube - Mini Youtube",
     src: YouTube,
     link: "https://tangerine-sawine-d8fbd2.netlify.app/",
     code: "https://github.com/Syed-Mohd-Azam/ReactTube--Mini-Youtube-Application",
   },
   {
-    name: "SnackSwift - Mini-Swiggy-App",
+    name: "SnackSwift - Mini Swiggy",
     src: Swiggy,
     link: "https://marvelous-boba-04d9c8.netlify.app/",
     code: "https://github.com/Syed-Mohd-Azam/SnackSwift---Mini-Swiggy-Application",
