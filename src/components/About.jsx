7c90f6264f5a4009c60e2f3a639f3b6555181d63
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-08 14:38:53 +0530

    About Component became Responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
       <section className="w-full h-screen flex justify-center items-center ">
         <section className="flex lg:flex-row max-w-7xl mx-auto lg:justify-end  items-center  flex-col  jutify-center gap-7">
           <article className="flex flex-col p-3 ">
-            <p className="text-3xl  lg:text-2xl mb-4 font-serif font-light italic tracking-widest text-opacity-85 text-indigo-800">
+            <p className="text-xl  lg:text-2xl mb-4 font-serif font-light italic tracking-widest text-opacity-85 text-indigo-800">
               Hi 👋 My name is
             </p>
-            <p className="text-5xl  whitespace-nowrap text-[#a5b4fc] lg:text-5xl mb-7 italic">
+            <p className="text-3xl  whitespace-nowrap text-[#a5b4fc] lg:text-5xl mb-7 italic">
               Syed Mohd Azam
             </p>
-            <p className="text-5xl  lg:text-5xl mb-5 font-mono text-indigo-800">
+            <p className="text-3xl  lg:text-5xl mb-5 font-mono text-indigo-800">
               {text}
             </p>
           </article>
