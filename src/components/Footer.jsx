ad2bcaef5ad13b10fed65d021231818f5e377c92
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-11 01:36:57 +0530

    Footer Component became Dark Mode Responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,15 +14,33 @@ const Footer = () => {
             : " w-full bg-[#121212] py-6 px-16 flex flex-col gap-3 lg:flex-row lg:justify-between  items-center lg:gap-20  shadow-xl"
         }
       >
-        <h1 className="text-white lg:text-2xl font-semibold tracking-wider italic">
+        <h1
+          className={
+            dark === false
+              ? "text-white lg:text-2xl font-semibold tracking-wider italic"
+              : "text-[#a5b4fc] lg:text-2xl font-semibold tracking-wider italic"
+          }
+        >
           &#169; No Copyright
         </h1>
-        <h1 className="text-white lg:text-2xl font-semibold tracking-wider italic">
+        <h1
+          className={
+            dark === false
+              ? "text-white lg:text-2xl font-semibold tracking-wider italic"
+              : "text-[#a5b4fc] lg:text-2xl font-semibold tracking-wider italic"
+          }
+        >
           🧡 Made with Love !! 🧡
         </h1>
 
         <section>
-          <p className="text-xl font-caveat text-white lg:text-4xl ml-3">
+          <p
+            className={
+              dark === false
+                ? "text-xl font-caveat text-white lg:text-4xl ml-3"
+                : "text-xl font-caveat text-[#a5b4fc] lg:text-4xl ml-3"
+            }
+          >
             <span className="pr-1">Syed</span>
             <span className="pr-1 ">Mohd</span>
             <span className="pr-1">Azam</span>
@@ -31,7 +49,7 @@ const Footer = () => {
                 className={
                   dark === false
                     ? "p-2 bg-white text-[#a5b4fc] rounded-full ml-4 hover:cursor-pointer font-semibold animate-bounce inline-block "
-                    : "p-2 bg-white text-[#121212] rounded-full ml-4 hover:cursor-pointer font-semibold animate-bounce inline-block "
+                    : "p-2 bg-[#a5b4fc] text-[#121212] rounded-full ml-4 hover:cursor-pointer font-semibold animate-bounce inline-block "
                 }
                 onClick={() => scrollToTop()}
               >
