b00fb33d24365239fc5f5d020a08e8150cc54364
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-22 05:34:11 +0530

    Project Added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,10 @@ import YouTube from "../assets/Youtube-Scrrenshot.png";
 import Swiggy from "../assets/Swiggy.png";
 const projects = [
   {
-    name: "ReactTube - Mini Youtube",
+    name: "ReactTube Player - Video Player Application",
     src: YouTube,
-    link: "https://tangerine-sawine-d8fbd2.netlify.app/",
-    code: "https://github.com/Syed-Mohd-Azam/ReactTube--Mini-Youtube-Application",
+    link: "https://mellow-piroshki-4e2159.netlify.app/",
+    code: "https://github.com/Syed-Mohd-Azam/Video-Player-Project",
   },
   {
     name: "SnackSwift - Mini Swiggy",
