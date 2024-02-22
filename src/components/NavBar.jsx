e9e2bcc38fe3d640dafcf7589f16c06d4325b7d6
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-23 03:28:32 +0530

    Navbar and Footer Light Color Changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
       <section
         className={
           dark === false
-            ? "fixed w-full  flex justify-between items-center px-12 py-5 bg-[#4B0082] z-50 shadow-md"
+            ? "fixed w-full  flex justify-between items-center px-12 py-5 bg-[#9A4EAE] z-50 shadow-md"
             : "fixed w-full  flex justify-between items-center px-12 py-5 bg-[#003153] z-50 shadow-md"
         }
       >
