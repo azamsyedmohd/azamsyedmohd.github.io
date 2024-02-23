d845eff0714cdeeed55e4c1e6055b4a6a2a16193
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-02-24 02:08:53 +0530

    Projects List Updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 import YouTube from "../assets/Youtube-Scrrenshot.png";
 import VideoPlayer from "../assets/Video-Player.png";
 import Swiggy from "../assets/Swiggy.png";
+import Disney from "../assets/Disney.PNG"
 const projects = [
   {
     name: " Video Player Application ",
@@ -9,6 +10,12 @@ const projects = [
     link: "https://mellow-piroshki-4e2159.netlify.app/",
     code: "https://github.com/Syed-Mohd-Azam/Video-Player-Project",
   },
+  {
+    name: "Flims Explorer",
+    src: Disney,
+    link: "https://vermillion-tapioca-211c42.netlify.app/" ,
+    code : "https://github.com/Syed-Mohd-Azam/Disney-Clone",
+  }
   {
     name: "SnackSwift - Mini Swiggy",
     src: Swiggy,
