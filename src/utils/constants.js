a908595f125585d5a8bc1b3a2df01a6d2992d4b5
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-04-17 01:21:03 +0530

    Project Almost Completed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 // AlL constants are there and can be imported whenever needed.
 import VideoPlayer from "../assets/Video-Player.png";
-import Swiggy from "../assets/Swiggy.png";
-import Disney from "../assets/DisneyHotstar.png";
+import ECommerce from "../assets/Ecommerce.PNG";
 import Weather from "../assets/weather.png";
 const projects = [
   {
@@ -10,24 +9,17 @@ const projects = [
     link: "https://mellow-piroshki-4e2159.netlify.app/",
     code: "https://github.com/Syed-Mohd-Azam/Video-Player-Project",
   },
-
+  {
+    name: "E-Commerce Project",
+    src: ECommerce,
+    link: "https://extraordinary-belekoy-3bc1ec.netlify.app/",
+    code: "https://github.com/smohdazam/smohdazam.github.io",
+  },
   {
     name: "Weather Application",
     src: Weather,
     link: "https://glittering-otter-4a9d76.netlify.app/",
     code: "https://github.com/Syed-Mohd-Azam/Weather-Application",
-  },
-  {
-    name: "Flims Explorer",
-    src: Disney,
-    link: "https://vermillion-tapioca-211c42.netlify.app/",
-    code: "https://github.com/Syed-Mohd-Azam/Disney-Clone",
-  },
-  {
-    name: "SnackSwift",
-    src: Swiggy,
-    link: "https://marvelous-boba-04d9c8.netlify.app/",
-    code: "https://github.com/Syed-Mohd-Azam/SnackSwift---Mini-Swiggy-Application",
   },
 ];
 export default projects;
